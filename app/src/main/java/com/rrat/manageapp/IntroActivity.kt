09a00c2635b79f61a680058803f2c083ed715f41
alvar <alvaro.monserrat@gmail.com>
2021-02-04 22:29:54 -0300

    Activity INTRO SIGN IN

--- app/src/main/java/com/rrat/manageapp/IntroActivity.kt
+++ app/src/main/java/com/rrat/manageapp/IntroActivity.kt
@@ -1,34 +1,28 @@
 package com.rrat.manageapp
 
-import android.content.Intent
 import android.graphics.Typeface
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.os.Looper
 import android.view.WindowInsets
 import android.view.WindowManager
-import com.rrat.manageapp.databinding.ActivitySplashBinding
+import com.rrat.manageapp.databinding.ActivityIntroBinding
 
-class SplashActivity : AppCompatActivity() {
+class IntroActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySplashBinding
+    private lateinit var binding: ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySplashBinding.inflate(layoutInflater)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setWindowFullScreen()
 
-        val typeFace: Typeface = Typeface.createFromAsset(assets, "pricedown bl.ttf")
-        binding.textViewAppName.typeface = typeFace
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        }, 2500)
+
 
     }
 
