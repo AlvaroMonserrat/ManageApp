fbfe3283cafcf79d9c9a078375de122852b224d0
alvar <alvaro.monserrat@gmail.com>
2021-02-11 21:18:49 -0300

    Sign Up Registrar email y constraseña

--- app/src/main/java/com/rrat/manageapp/activities/SignUpActivity.kt
+++ app/src/main/java/com/rrat/manageapp/activities/SignUpActivity.kt
@@ -7,6 +7,8 @@ import android.text.TextUtils
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.rrat.manageapp.R
 import com.rrat.manageapp.databinding.ActivitySignUpBinding
 
@@ -31,8 +33,28 @@ class SignUpActivity : BaseActivity() {
         val password: String = binding.etPassword.text.toString().trim{ it <= ' ' }
 
         if(validateForm(name, email, password)){
-            Toast.makeText(this, "Now we can register a new user.", Toast.LENGTH_SHORT).show()
-        }
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance()
+                    .createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        task ->
+                        hideProgressDialog()
+                        if(task.isSuccessful){
+                            val firebaseUser : FirebaseUser = task.result!!.user!!
+                            val registeredEmail = firebaseUser.email!!
+                            Toast.makeText(this, "$name you " +
+                                            "succesfully registered " +
+                                            "the email address $registeredEmail",
+                                    Toast.LENGTH_LONG)
+                                    .show()
+                            FirebaseAuth.getInstance().signOut()
+                            finish()
+                        }else{
+                            Toast.makeText(this, task.exception!!.message,
+                                    Toast.LENGTH_SHORT)
+                                    .show()
+                        }
+            }        }
 
 
     }
