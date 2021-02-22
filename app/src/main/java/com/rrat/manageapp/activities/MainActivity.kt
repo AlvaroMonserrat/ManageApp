f8f2db48e1040f2b02fff7a02de367a0a4fb605c
alvar <alvaro.monserrat@gmail.com>
2021-02-21 23:04:36 -0300

    Navigation View 90%

--- app/src/main/java/com/rrat/manageapp/activities/MainActivity.kt
+++ app/src/main/java/com/rrat/manageapp/activities/MainActivity.kt
@@ -3,10 +3,23 @@ package com.rrat.manageapp.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rrat.manageapp.R
+import com.rrat.manageapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(binding.drawerLayout.findViewById(R.id.toolbar_main_activity))
     }
 }
