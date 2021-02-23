1468f497c3ac26c2925675efed16acbe5cda23da
alvar <alvaro.monserrat@gmail.com>
2021-02-22 21:44:49 -0300

    Cargar Imagen en Header Navigation Viewer usando Glide

--- app/src/main/java/com/rrat/manageapp/activities/MainActivity.kt
+++ app/src/main/java/com/rrat/manageapp/activities/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.rrat.manageapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.core.view.GravityCompat
+import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.rrat.manageapp.R
 import com.rrat.manageapp.databinding.ActivityMainBinding
+import com.rrat.manageapp.firebase.FireStoreClass
+import com.rrat.manageapp.models.User
+import org.w3c.dom.Text
+import kotlin.math.log
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -16,10 +29,71 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        setupActionBar()
+
+        binding.navView.setNavigationItemSelectedListener(this)
+
+        FireStoreClass().signInUser(this)
 
     }
 
     private fun setupActionBar(){
-        setSupportActionBar(binding.drawerLayout.findViewById(R.id.toolbar_main_activity))
+
+        val toolBar = binding.drawerLayout.findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_main_activity)
+        
+        setSupportActionBar(toolBar)
+        toolBar.setNavigationIcon(R.drawable.ic_action_navigation_menu)
+
+        toolBar.setNavigationOnClickListener {
+            //Toggle Drawer
+            toggleDrawer()
+        }
+    }
+
+    private fun toggleDrawer(){
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+    }
+
+    override fun onBackPressed(){
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START)){
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            doubleBackToExit()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.nav_my_profile ->{
+                Toast.makeText(this, "My profile", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_sign_out->{
+                Toast.makeText(this, "My Sign Out", Toast.LENGTH_SHORT).show()
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, IntroActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+
+        return true
+    }
+
+    fun updateNavigationUserDetails(user: User) {
+        Glide
+                .with(this)
+                .load(user.image)
+                .centerCrop()
+                .placeholder(R.drawable.ic_user_place_holder)
+                .into(binding.drawerLayout.findViewById(R.id.nave_user_image));
+
+        binding.drawerLayout.findViewById<TextView>(R.id.tv_username).text = user.name
     }
 }
--- app/src/main/java/com/rrat/manageapp/firebase/FireStoreClass.kt
+++ app/src/main/java/com/rrat/manageapp/firebase/FireStoreClass.kt
@@ -1,9 +1,12 @@
 package com.rrat.manageapp.firebase
 
+import android.app.Activity
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
+import com.rrat.manageapp.activities.BaseActivity
+import com.rrat.manageapp.activities.MainActivity
 import com.rrat.manageapp.activities.SignInActivity
 import com.rrat.manageapp.activities.SignUpActivity
 import com.rrat.manageapp.models.User
@@ -34,17 +37,36 @@ class FireStoreClass {
         return currentUserId
     }
 
-    fun signInUser(activity: SignInActivity){
+    fun signInUser(activity: Activity){
         mFireStore.collection(Constants.USERS)
                 .document(getCurrentUserId())
                 .get()
                 .addOnSuccessListener { document->
                     val loggedInUser = document.toObject(User::class.java)
-                    if(loggedInUser != null)
-                        activity.signInSuccess(loggedInUser)
+                    if(loggedInUser != null){
+                        when(activity){
+                            is SignInActivity->{
+                                activity.signInSuccess(loggedInUser)
+                            }
+                            is MainActivity->{
+                                activity.updateNavigationUserDetails(loggedInUser)
+                            }
+                        }
+                    }
+
+
+
 
                 }.addOnFailureListener {
                     e->
+                    when(activity){
+                        is SignInActivity->{
+                            activity.hideProgressDialog()
+                        }
+                        is MainActivity->{
+                            activity.hideProgressDialog()
+                        }
+                    }
                     Log.e(activity.javaClass.simpleName, "Error: {$e}")
                 }
     }
